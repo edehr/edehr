2486e1645384ab0d4066a2b25a08a2a0e8c8ae88
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-03-05 14:45:04 -0800

    Link Vital data to chart and table

--- client/src/inside/components/Vitals.vue
+++ client/src/inside/components/Vitals.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   div(class="content")
-    button(id="addData", v-on:click="addData") Add data
     tabs
       tab(name="Table")
-        div {{ vitals }}
+        ehr-page-table(:tableDef="tableDef", :ehrHelp="ehrHelp", :pageDataKey="pageDataKey")
       tab(name="Chart",:selected="true")
-        vitals-chart(v-bind:vitals="vitals.table", v-bind:vitalsModel="vitalsModel")
+        vitals-chart(v-bind:vitals="tableData", v-bind:vitalsModel="vitalsModel")
+    div(style="display:none") {{refreshData}}
 </template>
 
 <script>
@@ -13,25 +13,49 @@ import Tabs from './Tabs'
 import Tab from './Tab'
 import VitalsChart from './VitalsChart'
 import VitalModel from '../../helpers/vitalModel'
+import EhrPageTable from '../components/EhrPageTable'
 
 export default {
   name: 'home',
   components: {
     Tabs,
     Tab,
+    EhrPageTable,
     VitalsChart
   },
   data () {
     return {
+      theData: {},
+      tableData: [],
       vitals: {
         table: []
       },
       vitalsModel: {}
     }
   },
+  props: {
+    pageDataKey: {type: String  }
+  },
+  computed: {
+    uiProps () {
+      return this.ehrHelp ? this.ehrHelp.getPageDefinition(this.pageDataKey) : {}
+    },
+    refreshData () {
+      this.refresh()
+      return this.tableData
+    },
+    tableDef () {
+      return this.uiProps.tables[0] || {}
+    }
+  },
   methods: {
-    addData () {
-      this.vitalsModel.addData(this.vitals.table)
+    refresh () {
+      let tableKey = this.tableDef.tableKey
+      let pageKey = this.pageDataKey
+      // console.log('EhrPageTable refresh for page table key', pageKey, tableKey)
+      let pageData = this.ehrHelp.getAsLoadedPageData(pageKey)
+      // store the current data into local data property for display
+      this.tableData = pageData[tableKey]
     }
   },
   created: function () {
