d6df51c9b3582c8704c5ab3d011a76bbe75df707
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-04-30 13:48:21 -0700

    Deployment setup

--- server/src/config/env/development.js
+++ server/src/config/env/development.js
@@ -1,12 +1,16 @@
 'use strict'
 
+/*
+API_URL=http://42.93.148.22/api CLIENT_URL=http://142.93.148.22 PORT=27004  npm run start-server
+API_URL=http://edehr.mac/api CLIENT_URL=http://edehr.mac npm run start-server
+ */
 var defaultEnvConfig = require('./default')
 const databaseName = 'edehr-dev'
 module.exports = {
   port: process.env.PORT || 27000,
-  apiUrl: 'http://localhost:27000',
+  apiUrl: process.env.API_URL || 'http://localhost:27000',
   // clientUrl is the url for this server that the Ed EHR client uses to make API calls
-  clientUrl: 'http://localhost:28000',
+  clientUrl:  process.env.CLIENT_URL || 'http://localhost:28000',
   defaultConsumerKey: 'edehrkey',
   traceApiCalls: true,
 
