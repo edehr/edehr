dfe7c0c4d524ab18b45d78a2783ab8a7d4ddcba1
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-06-07 07:54:52 -0700

    Security update, fix docs submodule, update tests

--- server/test/controllers/assignmentController.spec.js
+++ server/test/controllers/assignmentController.spec.js
@@ -2,11 +2,17 @@ var should = require('should')
 const mongoose = require('mongoose')
 import AssignmentController from '../../src/controllers/assignment-controller'
 import Helper from '../helper'
+import Config from '../../src/config/config'
+const config = new Config('test')
+const configuration = config.config
 const helper = new Helper()
 
 const typeName = 'AssignmentController'
 const modelName = 'Assignment'
 
+function makeAssignmentController() {
+  return new AssignmentController(configuration)
+}
 
 /* global describe it */
 describe(`${typeName} controller testing`, function () {
@@ -15,7 +21,7 @@ describe(`${typeName} controller testing`, function () {
   })
 
   it(`${typeName} be valid with model and key`, function (done) {
-    let m = new AssignmentController()
+    let m = makeAssignmentController()
     m.should.have.property('locateAssignmentForStudent')
     done()
   })
@@ -24,7 +30,7 @@ describe(`${typeName} controller testing`, function () {
   let toolConsumerId
 
   it(`${typeName} create model`, function (done) {
-    let m = new AssignmentController()
+    let m = makeAssignmentController()
     let data = Helper.sampleAssignmentSpec(undefined, key)
     toolConsumerId = data.toolConsumer
     m.create(data)
@@ -40,7 +46,7 @@ describe(`${typeName} controller testing`, function () {
   })
 
   it(`${typeName} use locateAssignmentForStudent`, function (done) {
-    let m = new AssignmentController()
+    let m = makeAssignmentController()
     m.locateAssignmentForStudent(key, toolConsumerId)
       .then(doc => {
         // console.log('results', doc)
