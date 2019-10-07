88d9bca9e68d2990cadc442e903aca0fd0353ac9
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-10-07 14:49:49 -0700

    place docs into BCcampus github.io page
    and  make small improvement to error messages on server

--- docDeploy.sh
+++ docDeploy.sh
@@ -22,6 +22,8 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:bryan-gilbert/edehr.git master:gh-pages
+# git push -f git@github.com:bryan-gilbert/edehr.git master:gh-pages
+
+git push -f git@github.com:BCcampus/edehr.git master:gh-pages
 
 cd -
