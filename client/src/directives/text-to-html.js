3776e1d7560e871a126a5cb3e9089592a0366c41
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-10-10 07:47:00 -0700

    Activity descriptions, text to html directive
    Fix the descriptions on the activity (dashboard). They were not showing up because the text-to-html directive only handled bind and not update.
    Fix the directive too.
    Change layout on Activity to arrange content in table so the row headers are more clearly visible

--- client/src/directives/text-to-html.js
+++ client/src/directives/text-to-html.js
@@ -1,29 +1,38 @@
 
 const debug = false
 
+function linky (text) {
+  let re = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[\/[a-zA-Z0-9()@:%_\-\+.~#?&=]*]*/g
+  let linkedText = text.replace(re,'<a href="$&" target="_blank">$&</a>')
+  if(debug) console.log('T2H linky', text, linkedText)
+  return linkedText
+}
+
+function worker (el, binding, source) {
+  if (debug) console.log('T2H directive source' , source, binding)
+  if (binding.value) {
+    if (debug) console.log('T2H directive binding', binding.value)
+    let sentences = binding.value.split('\n')
+    let html = []
+    let seenOne = false
+    html.push('<p>')
+    sentences.forEach(s => {
+      if (seenOne) html.push('<br/>')
+      html.push(linky(s))
+      seenOne = true
+    })
+    html.push('</p>')
+    el.innerHTML = html.join('\n')
+    if (debug) console.log('T2H el.innerHTML', el.innerHTML)
+  }
+}
+
 export default {
   bind (el, binding) {
-    function linky (text) {
-      let re = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[\/[a-zA-Z0-9()@:%_\-\+.~#?&=]*]*/g
-      let linkedText = text.replace(re,'<a href="$&" target="_blank">$&</a>')
-      if(debug) console.log('rich text linky', text, linkedText)
-      return linkedText
-    }
-    if (binding.value) {
-      if (debug) console.log('rich text directive binding', binding.value)
-      let sentences = binding.value.split('\n')
-      let html = []
-      let seenOne = false
-      html.push('<p>')
-      sentences.forEach(s => {
-        if (seenOne) html.push('<br/>')
-        html.push(linky(s))
-        seenOne = true
-      })
-      html.push('</p>')
-      el.innerHTML = html.join('\n')
-      if (debug) console.log('rich text el.innerHTML', el.innerHTML)
-    }
+    worker(el,binding, 'bind')
+  },
+  update (el, binding) {
+    worker(el,binding, 'update')
   }
 
 }
