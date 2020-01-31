ceceaddb77e5d40e1a7dfbb3aa0e60901a9668a0
Vinicius Arré <viniciusarre@gmail.com>
2020-01-31 17:33:41 -0300

    Chore: Remove blankline

--- client/src/inside/components/page/EhrElementForm.vue
+++ client/src/inside/components/page/EhrElementForm.vue
@@ -23,7 +23,7 @@
 
     div(v-else-if="isType('date')", class="date_wrapper")
       ehr-page-form-label(:element="element", css="date_label")
-        date-picker(class="d-picker", typeable, v-bind:disabled="disabled", v-bind:name="elementKey", v-model="inputVal")
+      date-picker(class="d-picker", typeable, v-bind:disabled="disabled", v-bind:name="elementKey", v-model="inputVal")
         //div(v-if="!showLabel", slot="beforeCalendarHeader", class="datepicker-header") {{label}}
 
     div(v-else-if="isType('day')", class="day_wrapper")
