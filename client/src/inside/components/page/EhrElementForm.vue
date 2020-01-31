183357f5fbf4eb3a1ea1d84f6d75df3dc38b9570
Vinicius Arré <viniciusarre@gmail.com>
2020-01-31 17:32:59 -0300

    Chore: revert required fields

--- client/src/inside/components/page/EhrElementForm.vue
+++ client/src/inside/components/page/EhrElementForm.vue
@@ -23,7 +23,6 @@
 
     div(v-else-if="isType('date')", class="date_wrapper")
       ehr-page-form-label(:element="element", css="date_label")
-      div(class="date-container")
         date-picker(class="d-picker", typeable, v-bind:disabled="disabled", v-bind:name="elementKey", v-model="inputVal")
         //div(v-if="!showLabel", slot="beforeCalendarHeader", class="datepicker-header") {{label}}
 
@@ -54,12 +53,6 @@
     div(v-else) ELSE: {{inputType}} {{label}}
 
 </template>
-
-<style scoped>
-.date-container {
-    min-height: 29vh;
-  }
-</style>
 
 <script>
 // import the common component with the .vue extension so that the base component doesn't need to have a template section
