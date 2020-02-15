48f093a6865718688246c42123fad7bf1525027d
Vinícius Eduardo da Silva Arré <viniciusarre@users.noreply.github.com>
2020-02-15 12:24:25 -0300

    Improve textarea display after saving in the save and next / previous flow (#558)
    * 383-2 Warn instructors when evaluation notes aren't saved
    
    * Chore: improve save and next/previous flow
    
    Co-authored-by: Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>

--- client/src/inside/components/EhrEvaluationInput.vue
+++ client/src/inside/components/EhrEvaluationInput.vue
@@ -1,28 +1,50 @@
 <template lang="pug">
-  div(class="ehr-eval-input")
-    div(class="evaluation-notes")
-      textarea(v-model="theNotes")
-    div(class="evaluation-controls")
-      ui-button(v-on:buttonClicked="cancelEvaluationNotes", :disabled="!enableActions", v-bind:secondary="true") Cancel
-      ui-button(v-on:buttonClicked="saveEvaluationNotes('saveNext')", :disabled="disableNext", class="is-pulled-right")  Save and next student
-      ui-button(v-on:buttonClicked="saveEvaluationNotes('saved')", :disabled="!enableActions", class="is-pulled-right")  Save
+  div
+    div(class="ehr-eval-input")
+      div(class="evaluation-notes")
+        textarea(v-model="theNotes")
+      div(class="evaluation-controls")
+        ui-button(v-on:buttonClicked="cancelEvaluationNotes", :disabled="!enableActions", v-bind:secondary="true") Cancel
+        ui-button(v-on:buttonClicked="saveEvaluationNotes('saveNext')", :disabled="disableNext", class="is-pulled-right")  Save and next student
+        ui-button(v-on:buttonClicked="saveEvaluationNotes('saved')", :disabled="!enableActions", class="is-pulled-right")  Save
+    ui-confirm(ref="confirmDialog", @confirm="handleConfirm", @abort="handleReset", @cancel="handleReset" set-footer)
 </template>
 
 <script>
 import UiButton from '../../app/ui/UiButton'
+import UiConfirm from '../../app/ui/UiConfirm'
 import StoreHelper from '../../helpers/store-helper'
+
+const HEADER = 'Your evaluation notes have not been saved'
+const TEXT = 'Would you like to...?'
+const CONFIRM =  {
+  beforeRouteLeave: 'Save and Continue',
+  previous: 'Save and previous',
+  next: 'Save and Next',
+}
+const CANCEL = 'Discard notes and continue'
+const LEAVE_PROMPT = 'Your evaluation notes have not been saved'
 
 export default {
   name: 'EhrEvaluationInput',
-  components: { UiButton },
+  components: {
+    UiButton,
+    UiConfirm 
+  },
   data: function () {
     return {
       theNotes: '',
-      asStored: ''
+      asStored: '',
+      proceed: false,
+      confirmStep: '',
+      next: null
     }
   },
+  
   props: {
-    enableNext: { type: Boolean }
+    enableNext: { type: Boolean },
+    shouldConfirmNavigation: {type: Boolean},
+    navigation: { type: String }
   },
   computed: {
     enableActions () {
@@ -33,16 +55,52 @@ export default {
     },
     asStoredEvaluationNotes () {
       return StoreHelper.getEvaluationNotes()
+    },
+    hasNewData () {
+      return (
+        this.theNotes && 
+        this.theNotes.length > 0 && 
+        this.theNotes !== this.asStoredEvaluationNotes
+      )
     }
   },
-  mounted: function (){
+  mounted: function (){ 
     this.loadDialog()
   },
+
+  created () {
+    this._setupEventHandlers()
+  },
+
+  beforeRouteLeave : function (to, from, next) {
+    console.log('beforeRouteLeave EhrEvaluationInput')
+    if(to.meta.layout !== 'inside' && this.hasNewData) {
+      this.confirmStep = 'beforeRouteLeave'
+      this.next = next
+      this.showConfirmationDialog()
+    } else {
+      next(true)
+    }
+  },
+
+  beforeDestroy () {
+    this._takeDownEventHandlers()
+  },
+
   watch: {
     asStoredEvaluationNotes: function (newData) {
       // console.log('EhrEvaluationInput watched')
       this.setNotes(newData)
-    }
+    },
+
+    shouldConfirmNavigation: function (shouldConfirm) {
+      if(shouldConfirm && this.hasNewData) {
+        this.confirmStep = 'navigation'
+        this.showConfirmationDialog()
+      } else {
+        this.$emit('proceedNavigation')
+      }
+    },  
   },
   methods: {
     setNotes (data) {
@@ -58,11 +116,73 @@ export default {
       this.$emit('canceled')
     },
     saveEvaluationNotes: function (event) {
-      return StoreHelper.saveEvaluationNotes(this.theNotes).then(() => {
-        this.$emit(event)
-      })
-    }
+      if (event) {
+        return StoreHelper.saveEvaluationNotes(this.theNotes).then(() => {
+          this.loadDialog()
+          this.$emit(event)
+        })
+      } else {
+        return StoreHelper.saveEvaluationNotes(this.theNotes)
+          .then(() => {
+            this.loadDialog()
+          })
+      }
+      
+    },
+
+    beforeUnloadListener (event) {
+      console.log('beforeUnloadListen')
+      let e = event || window.event
+      if (this.hasNewData) {
+        e.preventDefault()
+        e.returnValue = LEAVE_PROMPT
+      }
+    },
+
+
+    _setupEventHandlers () {
+      const _this = this
+      this.windowUnloadHandler = function (eData) {
+        _this.beforeUnloadListener(eData)
+      }
+      window.addEventListener('beforeunload', this.windowUnloadHandler)
+    },  
+
+    _takeDownEventHandlers (pageKey) {
+      window.removeEventListener('beforeunload', this.windowUnloadHandler)
+    },
+
+    handleReset () {
+      if(this.confirmStep === 'navigation') {
+        this.$emit('proceedNavigation')
+      } else if (this.confirmStep === 'beforeRouteLeave') {
+        this.next(true)
+      }
+      this.proceed = false
+      this.confirmStep = ''
+      this.setNotes('')
+    },
+
+    showConfirmationDialog () {
+      const key =  this.confirmStep === 'navigation' ? this.navigation : this.confirmStep     
+      this.$refs.confirmDialog.showDialog(HEADER, TEXT, CONFIRM[key], CANCEL)
+    },
+
+    async handleConfirm () {
+      await this.saveEvaluationNotes()
+      switch(this.confirmStep) {
+      case 'beforeRouteLeave':
+        this.next(true)
+        break
+      case 'navigation':
+        this.$emit('proceedNavigation')
+        break
+      default:
+        this.saveEvaluationNotes('saved')
+      }
+    },
   }
+  
 }
 </script>
 
