2dcbc5d19b4a6b66fb0febfe8d6e75d0e1c30281
Vinícius Arré <viniciusarre@gmail.com>
2020-03-10 11:14:03 -0300

    Chore: start implementing post request

--- server/test/scale-testing.js
+++ server/test/scale-testing.js
@@ -16,49 +16,14 @@ function getConsumers () {
   return axios.get(`${BASE_URL}/api/consumers`)
 }
 
-function launchLti (user, consumer, visit) {
-  if (visit) {
-    const ltiData = visit.ltiData[0]
-    return axios.get(`${BASE_URL}/launch_lti`, {
-      oauth_version: ltiData.oauth_version,
-      oauth_nonce: ltiData.oauth_nonce,
-      oauth_timestamp: ltiData.oauth_timestamp,
-      oauth_consumer_key: consumer.oauth_consumer_key,
-      user_id: user._id,
-      lis_person_sourcedid: ltiData.lis_person_sourcedid,
-      roles: consumer.roles,
-      context_id: ltiData.context_id,
-      context_label: ltiData.context_label,
-      context_title: ltiData.context_title,
-      resource_link_title: ltiData.context_label,
-      resource_link_description: ltiData.resource_link_description,
-      resource_link_id: ltiData.resource_link_id,
-      context_type: ltiData.context_type,
-      lis_course_section_sourcedid: ltiData.lis_course_section_sourcedid,
-      lis_result_sourcedid: ltiData.lis_result_sourcedid,
-      lis_outcome_service_url: ltiData.lis_outcome_service_url,
-      lis_person_name_given: ltiData.lis_person_name_given,
-      lis_person_name_family: ltiData.lis_person_name_family,
-      lis_person_name_full: ltiData.lis_person_name_full,
-      ext_user_username: ltiData.ext_user_username,
-      lis_person_contact_email_primary: ltiData.lis_person_contact_email_primary,
-      launch_presentation_locale: ltiData.launch_presentation_locale,
-      ext_lms: ltiData.ext_lms,
-      tool_consumer_info_product_family_code: consumer.tool_consumer_info_product_family_code,
-      tool_consumer_info_version: consumer.tool_consumer_info_version,
-      oauth_callback: ltiData.oauth_callback,
-      lti_version: ltiData.lti_version,
-      lti_message_type: ltiData.lti_message_type,
-      tool_consumer_instance_guid: consumer.tool_consumer_instance_guid,
-      tool_consumer_instance_name: consumer.tool_consumer_instance_name,
-      tool_consumer_instance_description: consumer.tool_consumer_instance_description,
-      custom_assignment: ltiData.custom_assignment,
-      launch_presentation_document_target: ltiData.launch_presentation_document_target,
-      launch_presentation_return_url: ltiData.launch_presentation_return_url,
-      oauth_signature_method: ltiData.oauth_signature_method,
-      oauth_signature: ltiData.oauth_signature,
-    })
-  }
+function launchLti () {
+  return axios.post(`${BASE_URL}/api/launch_lti`, {
+    oauth_customer_key: 'key',
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: Math.round(Date.now() / 1000),
+    oauth_nonce: Date.now() + Math.random() * 100,
+    custom_assignment: 'test_assignment_id_1'
+  })
 }
 
 Array.min = function(arr) {
@@ -73,27 +38,11 @@ let totalStart, totalEnd, diff = [], start, end
 
 async function main() {
     try {
-        let res = await getUsers()
-        const { users } = res.data
-        const USER_LENGTH = users.length
-        let parsedData = []
-        res = await getConsumers()
-        const { consumers } = res.data
-        res = await getVisits()
-        const { visits } = res.data
         totalStart = new Date()
-        for(let i = 0; i < 500; i++) { 
-            const index = Math.floor(Math.random() * (USER_LENGTH - 1))
-            const user = users[index]
-            console.log(`initiating request #${i + 1} with user_id: ${user._id}`)
-            let visit = visits.find(v => v.user === user._id)
-            if (visit) {
-                parsedData = visit.ltiData.map(d => JSON.parse(d))
-                visit = Object.assign({}, visit, { ltiData: parsedData })
-            }
-            consumer = consumers.find(c => c._id === user.toolConsumer)
+        for(let i = 0; i < 500; i++) {
             start = new Date()
-            await launchLti(user, consumer, visit, i)
+            const result = await launchLti()
+            console.log('result >> ', result)
             end = new Date()
             const timeDiff = (end.getTime() - start.getTime()) / 1000
             diff.push(timeDiff)
