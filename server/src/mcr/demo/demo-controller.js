68229d3f52776ce738cb330f133331b91f5c6fe1
Vinícius Arré <viniciusarre@gmail.com>
2020-05-12 12:50:40 -0300

    Chore: implement lazy creating the demo consumer

--- server/src/mcr/demo/demo-controller.js
+++ server/src/mcr/demo/demo-controller.js
@@ -1,32 +1,42 @@
 import { Router } from 'express'
-// import ToolConsumer from '../consumer/consumer'
+import ToolConsumer from '../consumer/consumer'
 import axios from 'axios'
 import Visit from '../visit/visit'
+import demoSeeder from '../../config/lib/demoSeeder'
 
 const HMAC_SHA1 = require('ims-lti/src/hmac-sha1')
 
-const DEMO_CONSUMER_SECRET = 'demosecret'
-const DEMO_CONSUMER_KEY = 'demokey'
-const DEMO_ASSIGNMENT = 'demoAssignment'
-const TOOL_CONSUMER_NAME = 'EdEHRDemo'
-const DEMO_CONTEXT_ID = 'demoContext'
+const DEMO_CONSUMER_SECRET = process.env.DEMO_CONSUMER_SECRET || 'demosecret'
+const DEMO_CONSUMER_KEY = process.env.DEMO_CONSUMER_KEY || 'demokey'
+const DEMO_ASSIGNMENT = process.env.DEMO_ASSIGNMENT || 'demoAssignment'
+const TOOL_CONSUMER_NAME = process.env.TOOL_CONSUMER_NAME || 'EdEHRDemo'
+const DEMO_CONTEXT_ID = process.env.DEMO_CONTEXT_ID || 'demoContext'
 
-const debug = true
+const debug = false
 
 export default class DemoController {
 
 
-  // isUniqueDemoUsername (req, res) {
-  //   const { username } = req.body
-  //   if (username) {
-  //     Visit.findOne({ userName: username, consumerKey: DEMO_CONSUMER_KEY})
-  //       .then(visit => {
-  //         res.status(200).json({ isUnique: !visit })
-  //       })
-  //   } else {
-  //     res.status(400).send('Invalid username')
-  //   }
-  // }
+  findOrCreateToolConsumer () {
+    return new Promise((resolve, reject) => {
+      return ToolConsumer.findOne({ 
+        oauth_consumer_key: DEMO_CONSUMER_KEY,
+        oauth_consumer_secret: DEMO_CONSUMER_SECRET
+      })
+        .then(consumer => {
+          // console.log('findOrcreateToolCOnsumer', consumer)
+          if (!consumer) {
+            demoSeeder()
+              .then(() => {
+                return this.findOrCreateToolConsumer()
+              })
+          } else {
+            resolve(consumer)
+          }
+        })
+    })
+    
+  }
 
   createDemoUser (req, res) {
     // const { fullName, role, email } = req.body
@@ -35,43 +45,48 @@ export default class DemoController {
     if (role) {
       // const names = fullName.split(' ')
       // const [firstName, lastName] = names
-      return this.generateUniqueName()
-        .then(userData => {
-          const ltiData = {
-            user_id: `${Math.floor(Date.now() / 1000)}`,
-            lis_person_name_given: userData.firstName,
-            lis_person_name_family: userData.lastName,
-            lis_person_name_full: userData.fullName,
-            lis_person_contact_email_primary: userData.email,
-            lti_version: 'LTI-1p0',
-            lti_message_type: 'basic-lti-launch-request',
-            roles: role,
-            oauth_consumer_key: DEMO_CONSUMER_KEY,
-            oauth_consumer_secret: DEMO_CONSUMER_SECRET,
-            context_id: DEMO_CONTEXT_ID,
-            tool_consumer_instance_name: TOOL_CONSUMER_NAME,
-            launch_presentation_return_url: 'http://some.place.org',
-            resource_link_title: 'Resource Link Title',
-            resource_link_id: 'http://link-to-resource.com/resource',
-            oauth_signature_method: 'HMAC-SHA1',
-            oauth_timestamp: Math.round(Date.now() / 1000),
-            oauth_nonce: Date.now() + Math.random() * 100,
-            custom_assignment: DEMO_ASSIGNMENT,
-          }
-    
-          const _req = this._signAndPrepareLTIRequest(ltiData, host)
-          this._LTIPost(_req)
-            .then((r) => {
-              console.log('res.data >> ', r.data)
-              res.status(200).json({ refreshToken: r.data.refreshToken, url: r.data.url })
-            })
-            .catch(err => {
-              if (debug) {
-                console.log('LTIPOST caught >> ', err.message)
-                res.status(500).send(err)
+      this.findOrCreateToolConsumer()
+        .then(consumer => {
+          if (debug) console.log('consumer >> ', consumer)
+          this.generateUniqueName()
+            .then(userData => {
+              const ltiData = {
+                user_id: `${Math.floor(Date.now() / 1000)}`,
+                lis_person_name_given: userData.firstName,
+                lis_person_name_family: userData.lastName,
+                lis_person_name_full: userData.fullName,
+                lis_person_contact_email_primary: userData.email,
+                lti_version: 'LTI-1p0',
+                lti_message_type: 'basic-lti-launch-request',
+                roles: role,
+                oauth_consumer_key: DEMO_CONSUMER_KEY,
+                oauth_consumer_secret: DEMO_CONSUMER_SECRET,
+                context_id: DEMO_CONTEXT_ID,
+                tool_consumer_instance_name: TOOL_CONSUMER_NAME,
+                // Return to demopage
+                launch_presentation_return_url: 'http://some.place.org',
+                resource_link_title: 'Resource Link Title',
+                resource_link_id: 'http://link-to-resource.com/resource',
+                oauth_signature_method: 'HMAC-SHA1',
+                oauth_timestamp: Math.round(Date.now() / 1000),
+                oauth_nonce: Date.now() + Math.random() * 100,
+                custom_assignment: DEMO_ASSIGNMENT,
               }
+      
+              const _req = this._signAndPrepareLTIRequest(ltiData, host)
+              this._LTIPost(_req)
+                .then((r) => {
+                  res.status(200).json({ refreshToken: r.data.refreshToken, url: r.data.url })
+                })
+                .catch(err => {
+                  if (debug) console.log('LTIPOST caught >> ', err.message)
+                  res.status(500).send(err)
+                })
             })
         })
+      
+       
+      // })
      
     } else {
       res.status(400).send('Invalid parameters')   
@@ -106,7 +121,7 @@ export default class DemoController {
         return res.data.results
       })
       .catch(err => {
-        console.log('error fetching names >> ', err)
+        if (debug) console.log('error fetching names >> ', err)
         Promise.reject(err)
       })
   }
@@ -120,7 +135,6 @@ export default class DemoController {
           const firstName = r.name.first
           const lastName = r.name.last
           const email = r.email
-
           if (fullName && firstName && lastName && email ) {
             Visit.findOne({
               consumerKey: DEMO_CONSUMER_KEY,
@@ -139,9 +153,7 @@ export default class DemoController {
             }))
           } else {
             reject('Name is not complete')
-          }
-
-          
+          }         
         }).catch(err => {
           reject(err)
         })
@@ -151,10 +163,6 @@ export default class DemoController {
 
   route () {
     const router = new Router()
-    // router.post('/isUniqueUsername', (req, res) => {
-    //   this.isUniqueDemoUsername(req, res)
-    // })
-
     router.post('/createUser', (req, res) => {
       this.createDemoUser(req, res)
     })
