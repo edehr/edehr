e9fd61f0cb59e78ad844f97e955da81cca436484
Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>
2020-05-28 19:32:46 -0700

    Adjust home page content (#700)
    * Adjust home page content
    
    * add email contact info

--- client/src/outside/views/Help.vue
+++ client/src/outside/views/Help.vue
@@ -2,10 +2,17 @@
   div(class="outside-view")
     h1 Help
     div
+      p.
+        The foundational work that created the prototype EdEHR was funded by the British Columbia Ministry of Advanced Education, Skills and Training through the agency of BCcampus.
+        The project was directed by a group of stakeholders from
+
+        Further funding has come from the British Columbia Institute of Technology (BCIT), University of British Columbia (UBC), University of Victoria
       p.  
         Are you a student having trouble with the app or need help with your assignment? Contact your instructor for help.
       p.
-        You can also read the project documentation:
+        If you need help or have a question about the EdEHR then send an email to <a href="mailto:info@edehr.org">info@edehr.org</a>
+      p.
+        You can also read the project documentation here:
       <ul>
         <li>  <a href="https://bccampus.github.io/edehr/student/">Student guide</a>
         </li>
