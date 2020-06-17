6824ec0a114428ec94453d0198381cb40c421283
Bryan Gilbert <bryan.gilbert@memsharp.com>
2020-06-17 13:43:11 -0700

    Client side tests
    Move directive and filter settings on main Vue instance into a helper so it can be shared with unit tests.
    Add data test ids to html elements to assist with testing.
    Add script to package.json to update snapshots
    Fix and augment the ClassList tests.

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import moment from 'moment'
 import router from './router'
 import store from './store'
 import icons from './icons'
 import homeLayout from './app/layout/LayoutHome.vue'
 import outsideLayout from './outside/layout/LayoutOutside.vue'
 import insideLayout from './inside/layout/LayoutEhr.vue'
-import dragDirective from './directives/drag-directive'
-import resizeDirective from './directives/resize-directive'
-import textToHtml from './directives/text-to-html'
-import validate from './directives/validate'
+import directivesFilters from './helpers/directives-filters'
 import VueAutosuggest from 'vue-autosuggest'
 import PageController from './helpers/page-controller'
 /*
@@ -24,26 +20,13 @@ import './scss/styles.scss'
 Configure Vue
  */
 icons(Vue)
+directivesFilters(Vue)
 Vue.config.productionTip = false
 Vue.use(require('vue-moment'))
 Vue.use(VueAutosuggest)
 Vue.component('home-layout', homeLayout)
 Vue.component('outside-layout', outsideLayout)
 Vue.component('inside-layout', insideLayout)
-Vue.directive('dragged', dragDirective)
-Vue.directive('resized', resizeDirective)
-Vue.directive('textToHtml', textToHtml) // used as text-to-html attribute
-Vue.directive('validate', validate)
-
-Vue.filter('formatDateDMY', function (value) {
-  return value ? moment(value).format('DD MMM YYYY') : ''
-})
-Vue.filter('formatDateYMD', function (value) {
-  return value ? moment(value).format('YYYY-MM-DD') : ''
-})
-Vue.filter('formatDateTime', function (value) {
-  return value ? moment(value).format('YYYY-MM-DD h:mm a') : ''
-})
 
 router.afterEach((to, from) => {
   PageController.onPageChange(to)
