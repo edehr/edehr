0f44016cb1d181ce95222171695f3a688ccb8b01
Vinícius Eduardo da Silva Arré <viniciusarre@users.noreply.github.com>
2020-07-23 14:52:53 -0300

    Implement tests for ehr-utils (#789)

--- client/src/helpers/test/ehr-utils.spec.js
+++ client/src/helpers/test/ehr-utils.spec.js
@@ -1,15 +1,21 @@
 const should = require('should')
+const mockData = require('./mockData.json')
+const { ehrUtilsData } = mockData
+jest.mock('axios')
+jest.mock('file-saver')
+
+
+function getOne () {
+  return ehrUtilsData.pageOne
+}
+function getTwo () {
+  return ehrUtilsData.pageTwo
+}
 // import should from 'should'
-import {
-  removeEmptyProperties,
-  ehrMarkSeed,
-  ehrRemoveMarkedSeed,
-  ehrMergeEhrData,
-  prepareAssignmentPageDataForSave,
-  validateSeedFileContents,
-  SEED_MARK
-} from '../ehr-utils'
+import * as ehrUtils from '../ehr-utils'
 import { Text } from '../ehr-text'
+import { createFile } from './testHelper'
+import { prepareAxiosResponse } from './axios-mock-helper'
 //
 describe('Test seed marking', () => {
 
@@ -21,21 +27,29 @@ describe('Test seed marking', () => {
     one.aPage1.should.have.property('nullProp')
     // should.exist(one.aPage1.nullProp)
     should.exist(one.aPage1.emptyString)
-    one = removeEmptyProperties(one)
+    one = ehrUtils.removeEmptyProperties(one)
     one.aPage1.should.not.have.property('emptyProp')
     one.aPage1.should.not.have.property('nullProp')
     should.not.exist(one.aPage1.emptyProp)
   })
 
+  it('decoupleObject', done => {
+    const one = getOne()
+    const result = ehrUtils.decoupleObject(one)
+    result.should.be.an.Object()
+    JSON.stringify(one).should.equal(JSON.stringify(one))
+    done()
+  })
+
   it('Mark seed', () => {
     let one = getOne()
-    one = ehrMarkSeed(one)
+    one = ehrUtils.ehrMarkSeed(one)
     should.exist(one.visit.location)
     one.visit.location.should.have.length(1)
     let v = one.visit.location[0]
-    v.should.have.property(SEED_MARK)
+    v.should.have.property(ehrUtils.SEED_MARK)
     let two = getTwo()
-    let pages = ehrMergeEhrData(one, two)
+    let pages = ehrUtils.ehrMergeEhrData(one, two)
     // verify we get pages from one or the other or both
     pages.should.have.property('aPage1')
     pages.should.have.property('cardiovascular')
@@ -47,23 +61,23 @@ describe('Test seed marking', () => {
   })
 
   it('Remove Marked Properties', () => {
-    let one = ehrMarkSeed(getOne())
+    let one = ehrUtils.ehrMarkSeed(getOne())
     let two = getTwo()
-    let pages = ehrMergeEhrData(one, two)
+    let pages = ehrUtils.ehrMergeEhrData(one, two)
     pages.visit.should.have.property('location')
     pages.visit.location.should.have.length(2)
-    let visit = ehrRemoveMarkedSeed(pages.visit)
+    let visit = ehrUtils.ehrRemoveMarkedSeed(pages.visit)
     visit.should.have.property('location')
     visit.location.should.have.length(1)
   })
 
   it('Prepare Assignment Data For Save', () => {
-    let one = ehrMarkSeed(getOne())
+    let one = ehrUtils.ehrMarkSeed(getOne())
     let two = getTwo()
-    let pages = ehrMergeEhrData(one, two)
+    let pages = ehrUtils.ehrMergeEhrData(one, two)
     pages.visit.should.have.property('location')
     pages.visit.location.should.have.length(2)
-    let page = prepareAssignmentPageDataForSave(pages.visit)
+    let page = ehrUtils.prepareAssignmentPageDataForSave(pages.visit)
     page.should.have.property('location')
     page.location.should.have.length(1)
   })
@@ -83,14 +97,14 @@ describe('Test merging two EHR data object', () => {
   it('Merge seed with empty object', () => {
     let one = getOne()
     let two = undefined
-    let pages = ehrMergeEhrData(one, two)
+    let pages = ehrUtils.ehrMergeEhrData(one, two)
     should.exist(pages)
   })
 
   it('Merge seed and assignment data', () => {
     let one = getOne()
     let two = getTwo()
-    let pages = ehrMergeEhrData(one, two)
+    let pages = ehrUtils.ehrMergeEhrData(one, two)
     // verify we get pages from one or the other or both
     pages.should.have.property('aPage1')
     pages.should.have.property('aPage2')
@@ -121,7 +135,7 @@ describe('Test support for seed import', () => {
   it('verify validateSeedFileContents accept valid properties', (done) => {
     let ehrData = {demographics: {firstName: 'foo'}}
     let data = composeData(ehrData)
-    let {invalidMsg,seedObj } = validateSeedFileContents(data)
+    let {invalidMsg,seedObj } = ehrUtils.validateSeedFileContents(data)
     should.not.exist(invalidMsg)
     should.exist(seedObj)
     done()
@@ -129,7 +143,7 @@ describe('Test support for seed import', () => {
   it('verify validateSeedFileContents catches invalid properties', (done) => {
     let ehrData = {invalid: {firstName: 'foo'}}
     let data = composeData(ehrData)
-    let {invalidMsg,seedObj } = validateSeedFileContents(data)
+    let {invalidMsg,seedObj } = ehrUtils.validateSeedFileContents(data)
     should.exist(invalidMsg)
     should.not.exist(seedObj)
     done()
@@ -137,12 +151,158 @@ describe('Test support for seed import', () => {
   it('verify validateSeedFileContents catches empty object', (done) => {
     let ehrData = {}
     let data = composeData(ehrData)
-    let {invalidMsg,seedObj } = validateSeedFileContents(data)
+    let {invalidMsg,seedObj } = ehrUtils.validateSeedFileContents(data)
     should.exist(invalidMsg)
     should.not.exist(seedObj)
     done()
   })
+
+  it('importSeedData', async done => {
+    should.doesNotThrow(async () => {
+      const seedId = 'new seed'
+      let ehrData = { demographics: { firstName: 'foo' } }
+      let data = composeData(ehrData)
+      const response = {
+        imported: true,
+        seeddata: mockData.seedData
+      }
+      await prepareAxiosResponse('get', response)
+      await prepareAxiosResponse('put', response)
+      should.doesNotThrow(async () => await ehrUtils.importSeedData(null, seedId, data))
+      done()
+    })
+  })
+
+  it('downloadSeedToFile', done => {
+    const seedId = 'new seed'
+    const sSeedContent = mockData.seedData
+    const ehrData = { demographics: { firstName: 'foo' } }
+    should.doesNotThrow(() => {
+      const result = ehrUtils.downloadSeedToFile(seedId, sSeedContent, ehrData)
+      should.not.exist(result)
+      done()
+    })
+  })
+
+  it('downObjectToFile', done => {
+    const fileName = 'new file'
+    const obj = mockData.seedData
+    should.doesNotThrow(() => {
+      const result = ehrUtils.downObjectToFile(fileName, obj)
+      should.not.exist(result)
+      done()
+    })
+  })
+
+  it('arrayToCsv', done => {
+    should.doesNotThrow(() => {
+      const array = [['A'], ['New'], ['Array']]
+      const result = ehrUtils.arrayToCsv(array)
+      result.should.be.a.String()
+      result.length.should.be.greaterThan(0)
+      done()
+    })
+  })
+
+  it('downArrayToCsvFile', () => {
+    should.doesNotThrow(() => {
+      const array = [['A'], ['New'], ['Array']]
+      const result = ehrUtils.downArrayToCsvFile('new file', array)
+      should.not.exist(result)
+    })
+  })
 })
+
+describe('Test validators and formatters ', () => {
+  it('validTimeStr', done => {
+    // invalid tests
+    ehrUtils.validTimeStr('2400').should.equal(false)
+    ehrUtils.validTimeStr('2200').should.equal(false)
+    ehrUtils.validTimeStr('0100').should.equal(false)
+    ehrUtils.validTimeStr('24:00').should.equal(false)
+    ehrUtils.validTimeStr('33:00').should.equal(false)
+    ehrUtils.validTimeStr('23:60').should.equal(false)
+    // valid tests    
+    ehrUtils.validTimeStr('00:00').should.equal(true)
+    ehrUtils.validTimeStr('01:00').should.equal(true)
+    ehrUtils.validTimeStr('22:00').should.equal(true)
+    ehrUtils.validTimeStr('23:59').should.equal(true)
+    done()
+  })
+
+  it('validDayStr', done => {
+    // invalid tests
+    ehrUtils.validDayStr(-1).should.equal(false)
+    ehrUtils.validDayStr(10).should.equal(false)
+    ehrUtils.validDayStr(100).should.equal(false)
+    ehrUtils.validDayStr(1000).should.equal(false)
+    
+    // valid tests
+    ehrUtils.validDayStr(0).should.equal(true)
+    ehrUtils.validDayStr(1).should.equal(true)
+    ehrUtils.validDayStr(5).should.equal(true)
+    ehrUtils.validDayStr(9).should.equal(true)
+    done()
+  })
+
+  it('validRangeStr', done => {
+    // invalid use cases
+    ehrUtils.validRangeStr(1000, 100, 0).should.equal(false)
+    ehrUtils.validRangeStr(-1000, 100, -100).should.equal(false)
+    ehrUtils.validRangeStr('a', 100).should.equal(false)
+    // valid use cases
+    ehrUtils.validRangeStr(10, 100, 0).should.equal(true)
+    ehrUtils.validRangeStr(-10, 100, -100).should.equal(true)
+    ehrUtils.validRangeStr(100, 1000, 0).should.equal(true)
+    done()
+  })
+
+  it('formatDateStr', done => {
+    ehrUtils.formatDateStr('10/11/2011').should.equal('11 Oct 2011')
+    ehrUtils.formatDateStr('04/01/2012').should.equal('01 Apr 2012')  
+    done()
+  })
+
+  it('formatTimeStr', done => {
+    ehrUtils.formatTimeStr('10/11/2011 12:00').should.equal('2011-10-11 12:00 pm')
+    ehrUtils.formatTimeStr('10/11/2011 15:00').should.equal('2011-10-11 3:00 pm')
+    ehrUtils.formatTimeStr('10/11/2011 00:00').should.equal('2011-10-11 12:00 am')
+    done()
+  })
+
+  it('isValidFilename', done => {
+    ehrUtils.isValidFilename('new/file/name').should.equal(false)
+    ehrUtils.isValidFilename('new-file-name').should.equal(true)
+    done()
+
+  })
+})
+
+describe('Axios error and file upload', () => {
+  it('composeAxiosResponseError', done => {
+    const response = { 
+      status: 400,
+      statusText: 'Bad Request',
+      data: 'Please, check your request and try again'
+    }
+
+    const result = ehrUtils.composeAxiosResponseError({response}, '')
+    result.should.equal(` status: ${response.status} ${response.statusText} ${response.data}`)
+    done()
+  })
+
+  it('readFile', async done => {
+    const fileContent = 'This is a mock file'
+    const file = createFile(fileContent)
+    should.exist(file)
+    const result = await ehrUtils.readFile(file) 
+    result.should.equal(fileContent)
+    done()
+  })
+
+ 
+})
+
 
 function composeData (ehrData) {
   let data = {
@@ -153,63 +313,3 @@ function composeData (ehrData) {
   return JSON.stringify(data,null,2)
 }
 
-
-function getOne () {
-  return {
-    aPage1: {
-      p1: 1,
-      emptyString: '',
-      nullProp: null,
-      emptyProp: undefined
-    },
-    visit: {
-      location: [{ location: 'in one', transferInDay: '1', transferInTime: '1' }],
-      admissionDay: 'Day 1',
-      admissionTime: '7:00am',
-      propertyInOne: 'propertyInOne',
-      status: 'From One'
-    },
-    cardiovascular: {
-      table: [
-        {
-          name: 'Jane',
-          profession: 'nurse',
-          day: '0',
-          time: '9:00',
-          pulse: 'Regular',
-          skinAppearance: 'Pale',
-          capRefillRightHand: 'Normal'
-        }
-      ]
-    }
-  }
-}
-function getTwo () {
-  return {
-    visit: {
-      location: [{ location: 'in two', transferInDay: '2' }],
-      admissionDay: 'Day 2',
-      admissionTime: '10:00am',
-      consentForTreatment: false,
-      propertyInTwo: 'propertyInTwo',
-      consentForBlood: true,
-      diagnosis: 'two diagnosis',
-      status: 'From two'
-    },
-    aPage2: {
-      p2: 2
-    },
-    cardiovascular: {
-      table: [
-        {
-          name: 'Joe',
-          profession: 'nurse',
-          day: '1',
-          time: '10:00',
-          pulse: 'Irregular',
-          skinAppearance: 'Dark'
-        }
-      ]
-    }
-  }
-}
