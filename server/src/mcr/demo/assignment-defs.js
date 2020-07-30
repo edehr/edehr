6ef2f7dec1699a8585e85b93ccd0ae48958fbe65
Bryan Gilbert <13558676+bryan-gilbert@users.noreply.github.com>
2020-07-30 16:15:58 -0700

    Fix #820 demo mode startup is broken (#821)
    https://github.com/BCcampus/edehr/issues/820

--- server/src/mcr/demo/assignment-defs.js
+++ server/src/mcr/demo/assignment-defs.js
@@ -8,7 +8,7 @@ export const assignment1 = {
   description: 'This demonstration assignment is based on Case Study #1.\n' +
   'In this case the patient has chronic obstructive pulmonary disease (COPD) with a history of asthma. The interprofessional collaboration is role modelled between nursing, medical radiology, medical laboratory, and healthcare workers in the emergency department. ' +
   'The focus for this assignment is help the student complement their assessment and monitoring skills with accurate and careful recording within an electronic health record system.',
-  title: 'Demonstration Case Study #1: COPD'
+  resource_link_title: 'Demonstration Case Study #1: COPD'
 }
 
 export const assignment2 = {
@@ -19,5 +19,5 @@ export const assignment2 = {
   day: 3,
   description: 'This demonstration assignment is based on Case Study #2.\n In this case the patient condition of COPD is exacerbation due to community acquired pneumonia. The patient in this case study has a complicated health history.' +
   'The focus for this assignment is help the student complement their assessment and monitoring skills with accurate and careful recording within an electronic health record system.',
-  title: 'Demonstration Case Study #2: Pneumonia'
+  resource_link_title: 'Demonstration Case Study #2: Pneumonia'
 }
