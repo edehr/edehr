17267211a289d0c5c81c96564ca48db8924656a4
Vinícius Arré <viniciusarre@gmail.com>
2020-08-12 10:38:11 -0300

    Add noMarRecords mocks and further divide mar-helper tests

--- client/src/inside/components/mar/tests/mar-helper.spec.js
+++ client/src/inside/components/mar/tests/mar-helper.spec.js
@@ -26,94 +26,201 @@ const getAsLoaded = (key) => {
   }
 }
 
+const noMarGetAsLoaded = (key) => {
+  if (key === MED_ORDERS_PAGE_KEY){
+    return Object.assign({}, {
+      pageElements: { table: { tableKey: 'table'}},
+      table: medOrders
+    })
+  } else if (key === MAR_PAGE_KEY) {
+    return Object.assign({}, {
+      pageElements: { table: { tableKey: 'table'}},
+      table: []
+    })
+  }
+}
+
 const mockedEhrHelper = {
   getAsLoadedPageData: getAsLoaded,
   _saveData: jest.fn()
 }
 
+let noMarEhrHelper = {}
 describe('mar-helper tests', () => {
-  it('properly instantiates the class', () => {
-    should.doesNotThrow(() => {
-      marHelper = new MarHelper(mockedEhrHelper)
-      should.exist(marHelper)
-    })
-  })
-
-  it('refreshMarData', () => {
-    should.doesNotThrow(() => {
-      marHelper.refreshMarData()
-    })
-  })
-
-  it('getEhrData_Orders', () => {
-    should.doesNotThrow(() => {
-      marHelper.getEhrData_Orders()
-      const theMedOrders = marHelper.theMedOrders
-      should.exist(theMedOrders)
-      theMedOrders.should.eql(classMedOrders)
-    })
-  })
-
-  it('getEhrData_MarRecords', () => {
-    should.doesNotThrow(() => {
-      marHelper.getEhrData_MarRecords()
-      const records = marHelper.marRecords
-      should.exist(records)
-      records.should.eql(classMarRecords)
-    })
-  })
-
-  it('getEhrData_MarPageData', () => {
-    should.doesNotThrow(() => {
-      const result = marHelper.getEhrData_MarPageData()
-      result.table.should.eql(marRecords)
-    })
-  })
-
-  it('clearAllData', () => {
-    should.doesNotThrow(() => {
-      marHelper.clearAllData()
-      const calls = mockedEhrHelper._saveData.mock.calls
-      calls.length.should.equal(1)   
-    })
-  })
-
-  it('getPageDef_Mar', () => {
-    should.doesNotThrow(() => {
-      const pageDef = EhrDefs.getPageDefinition(MAR_PAGE_KEY)
-      const result = marHelper.getPageDef_Mar()
-      result.should.eql(pageDef)
-    })
-  })
-
-  it('getMarTableKey', () => {
-    should.doesNotThrow(() => {
-      const pageKey = mockedEhrHelper.getAsLoadedPageData(MAR_PAGE_KEY).pageElements.table.tableKey
-      const result = marHelper.getMarTableKey()
-      result.should.equal(pageKey)
-    })
-  })
-
-  it('saveMarDialog', () => {
-    should.doesNotThrow(() => {
-      const newMarRecord = new MarEntity({
-        actualTime: '08:00',
-        comment: 'Test MAR',
-        day: 0,
-        medications: [
-          new MedOrder('acetylcysteine injection')
-        ]
+  describe('tests With MarRecords', () => {
+    it('properly instantiates the class', () => {
+      should.doesNotThrow(() => {
+        marHelper = new MarHelper(mockedEhrHelper)
+        should.exist(marHelper)
       })
-      marHelper.saveMarDialog(newMarRecord)
-      mockedEhrHelper._saveData.mock.calls.length.should.equal(2)
+    })
+  
+    it('refreshMarData', () => {
+      should.doesNotThrow(() => {
+        marHelper.refreshMarData()
+      })
+    })
+  
+    it('getEhrData_Orders', () => {
+      should.doesNotThrow(() => {
+        marHelper.getEhrData_Orders()
+        const theMedOrders = marHelper.theMedOrders
+        should.exist(theMedOrders)
+        theMedOrders.should.eql(classMedOrders)
+      })
+    })
+  
+    it('getEhrData_MarRecords', () => {
+      should.doesNotThrow(() => {
+        marHelper.getEhrData_MarRecords()
+        const records = marHelper.marRecords
+        should.exist(records)
+        records.should.eql(classMarRecords)
+      })
+    })
+  
+    it('getEhrData_MarPageData', () => {
+      should.doesNotThrow(() => {
+        const result = marHelper.getEhrData_MarPageData()
+        result.table.should.eql(marRecords)
+      })
+    })
+  
+    it('clearAllData', () => {
+      should.doesNotThrow(() => {
+        marHelper.clearAllData()
+        const calls = mockedEhrHelper._saveData.mock.calls
+        calls.length.should.equal(1)   
+      })
+    })
+  
+    it('getPageDef_Mar', () => {
+      should.doesNotThrow(() => {
+        const pageDef = EhrDefs.getPageDefinition(MAR_PAGE_KEY)
+        const result = marHelper.getPageDef_Mar()
+        result.should.eql(pageDef)
+      })
+    })
+  
+    it('getMarTableKey', () => {
+      should.doesNotThrow(() => {
+        const pageKey = mockedEhrHelper.getAsLoadedPageData(MAR_PAGE_KEY).pageElements.table.tableKey
+        const result = marHelper.getMarTableKey()
+        result.should.equal(pageKey)
+      })
+    })
+  
+    it('saveMarDialog', () => {
+      should.doesNotThrow(() => {
+        const newMarRecord = new MarEntity({
+          actualTime: '08:00',
+          comment: 'Test MAR',
+          day: 0,
+          medications: [
+            new MedOrder('acetylcysteine injection')
+          ]
+        })
+        marHelper.saveMarDialog(newMarRecord)
+        mockedEhrHelper._saveData.mock.calls.length.should.equal(2)
+      })
+    })
+  
+    it('triggerActionByPageKey', () => {
+      should.doesNotThrow(() => {
+        marHelper.triggerActionByPageKey()
+      })
     })
   })
 
-  it('triggerActionByPageKey', () => {
-    should.doesNotThrow(() => {
-      marHelper.triggerActionByPageKey()
+  describe('tests without MarRecord', () => {
+    beforeAll(() => {
+      noMarEhrHelper = {
+        getAsLoadedPageData: noMarGetAsLoaded,
+        _saveData: jest.fn()
+      }
+    })
+    it('properly instantiates the class', () => {
+      should.doesNotThrow(() => {
+        marHelper = new MarHelper(noMarEhrHelper)
+        should.exist(marHelper)
+      })
+    })
+  
+    it('refreshMarData', () => {
+      should.doesNotThrow(() => {
+        marHelper.refreshMarData()
+      })
+    })
+  
+    it('getEhrData_Orders', () => {
+      should.doesNotThrow(() => {
+        marHelper.getEhrData_Orders()
+        const theMedOrders = marHelper.theMedOrders
+        should.exist(theMedOrders)
+        theMedOrders.should.eql(classMedOrders)
+      })
+    })
+  
+    it('getEhrData_MarRecords', () => {
+      should.doesNotThrow(() => {
+        marHelper.getEhrData_MarRecords()
+        const records = marHelper.marRecords
+        should.exist(records)
+        records.length.should.equal(0)
+      })
+    })
+  
+    it('getEhrData_MarPageData', () => {
+      should.doesNotThrow(() => {
+        const result = marHelper.getEhrData_MarPageData()
+        result.table.length.should.eql(0)
+      })
+    })
+  
+    it('clearAllData', () => {
+      should.doesNotThrow(() => {
+        marHelper.clearAllData()
+        const calls = mockedEhrHelper._saveData.mock.calls
+        calls.length.should.equal(2)   
+      })
+    })
+  
+    it('getPageDef_Mar', () => {
+      should.doesNotThrow(() => {
+        const pageDef = EhrDefs.getPageDefinition(MAR_PAGE_KEY)
+        const result = marHelper.getPageDef_Mar()
+        result.should.eql(pageDef)
+      })
+    })
+  
+    it('getMarTableKey', () => {
+      should.doesNotThrow(() => {
+        const pageKey = mockedEhrHelper.getAsLoadedPageData(MAR_PAGE_KEY).pageElements.table.tableKey
+        const result = marHelper.getMarTableKey()
+        result.should.equal(pageKey)
+      })
+    })
+  
+    it('saveMarDialog', () => {
+      should.doesNotThrow(() => {
+        const newMarRecord = new MarEntity({
+          actualTime: '08:00',
+          comment: 'Test MAR',
+          day: 0,
+          medications: [
+            new MedOrder('acetylcysteine injection')
+          ]
+        })
+        marHelper.saveMarDialog(newMarRecord)
+        mockedEhrHelper._saveData.mock.calls.length.should.equal(2)
+      })
+    })
+  
+    it('triggerActionByPageKey', () => {
+      should.doesNotThrow(() => {
+        marHelper.triggerActionByPageKey()
+      })
     })
   })
-
 
 })
