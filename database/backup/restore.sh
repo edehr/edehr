1c37ac42d932b471aff484a60664ebe328684847
Bryan <bryan.gilbert@memsharp.com>
2022-03-11 12:00:03 -0800

    continue work on edehr scripts
    continue

--- database/backup/restore.sh
+++
@@ -1,1 +0,0 @@
-mongorestore --db edehr-dev ./dump/edehr-dev/
--- edehr.org/api_scripts/createDroplet.sh
+++ edehr.org/api_scripts/createDroplet.sh
@@ -1,2 +1,2 @@
 # Create a new droplet
-curl -X POST -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -H 'Content-Type: application/json' -d @drop-def.json "https://api.digitalocean.com/v2/droplets"
+curl -X POST -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -H 'Content-Type: application/json' -d @../.private/drop-def.json "https://api.digitalocean.com/v2/droplets"
