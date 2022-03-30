dbec2fdb76aefad52df63afba13e67f8a055bbd7
Bryan <bryan.gilbert@memsharp.com>
2022-03-29 20:53:16 -0700

    remove scripts better located elsewhere

--- edehr.org/api_scripts/createDroplet.sh
+++
@@ -1,2 +0,0 @@
-# Create a new droplet
-curl -X POST -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" -H 'Content-Type: application/json' -d @./env/drop-def.json "https://api.digitalocean.com/v2/droplets"
