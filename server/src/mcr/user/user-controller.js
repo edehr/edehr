462123da2e2503efdcc340283359c41e452adc30
Bryan <bryan.gilbert@memsharp.com>
2022-10-13 14:33:40 -0700

    display LObj name beside activity name
    On the course list, to compensate for a problem with D2L, we'll show the LObj name beside the activity name

--- server/src/mcr/user/user-controller.js
+++ server/src/mcr/user/user-controller.js
@@ -2,6 +2,7 @@ import BaseController from '../common/base'
 import User from './user'
 import Visit from '../visit/visit'
 import { ok, fail } from '../common/utils'
+import Activity from '../activity/activity'
 const ObjectID = require('mongodb').ObjectId
 const debug = require('debug')('server')
 
@@ -16,31 +17,41 @@ export default class UserController extends BaseController {
   */
 
   listAsInstructorCourses (id) {
-    debug('listAsInstructorCourses for ' + id)
-    return Visit.find({ $and: [ {isInstructor: true }, {user: new ObjectID(id)} ] })
-      .populate('activity', 'context_id context_label context_title resource_link_title resource_link_description assignment')
-      // .populate('assignment', 'name description seedDataId')
-      // .select('lastVisitDate activityData')
-      .then(visits => {
-        let courses = []
-        visits.forEach(visit => {
-          const activity = visit.activity
-          // console.log('activity', activity)
-          let cId = activity.context_id
-          let course = courses.find(c => c.id === cId)
-          if (!course) {
-            course = {
-              id: cId,
-              name: activity.context_title,
-              label: activity.context_label,
-              activities: []
-            }
-            courses.push(course)
-          }
-          course.activities.push(activity)
+    return new Promise((resolve) => {
+      debug('listAsInstructorCourses for ' + id)
+      Visit.find({ $and: [{ isInstructor: true }, { user: new ObjectID(id) }] })
+        .then(visits => {
+          let courses = []
+          const promiseList = visits.map(visit => {
+            console.log('visit', visit)
+            const activityId = visit.activity
+            console.log('find activityId', activityId)
+            return Activity.findById(activityId)
+              .populate('assignment', 'name description seedDataId')
+              .then(activity => {
+                console.log('as found activity', activity)
+                let cId = activity.context_id
+                let course = courses.find(c => c.id === cId)
+                console.log('course', course)
+                if (!course) {
+                  course = {
+                    id: cId,
+                    name: activity.context_title,
+                    label: activity.context_label,
+                    activities: []
+                  }
+                  console.log('course', course)
+                  courses.push(course)
+                }
+                course.activities.push(activity)
+              })
+          })
+          Promise.all(promiseList).then(() => {
+            console.log('return all the courses', courses)
+            resolve({ courses: courses })
+          })
         })
-        return { courses: courses }
-      })
+    })
   }
 
   route () {
