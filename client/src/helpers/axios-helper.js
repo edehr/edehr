e79960bd67a9bbb119cd749bd5f3fc0fecdbbac0
Bryan <bryan.gilbert@memsharp.com>
2022-10-14 00:41:35 -0400

    Fix class list iteration when evaluating,
    Fix seed editing, retain seed id while navigating ehr pages

--- client/src/helpers/axios-helper.js
+++ client/src/helpers/axios-helper.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import authHelper from '@/helpers/auth-helper'
 
 export const setAuthHeader = (token) => {
-  console.log('setAuthHeader', authHelper.hashToken(token))
+  //console.log('setAuthHeader', authHelper.hashToken(token))
   if (token) {
     axios.defaults.headers['Authorization'] = `Bearer ${token}`
   } else {
