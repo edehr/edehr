f27d99f7d0c51b12dc974af719fe63bc40dbce39
Bryan <bryan.gilbert@memsharp.com>
2023-02-05 12:34:38 -0800

    deployment env check fix

--- deployment/env-check.js
+++ deployment/env-check.js
@@ -2,10 +2,11 @@ const fs = require('fs')
 const path = require('path')
 const eFile = path.join(__dirname, '.env')
 const envContents = fs.readFileSync(eFile, 'utf-8')
+const envLines = envContents.split('\n').sort()
 
 const toCheck = [
   'API_PORT',
-  'API_LOG_FILES_DIRECTORY',
+  'API_TOKEN',
   'APP_TITLE',
   'APP_VERSION',
   'AUTH_TOKEN_SECRET',
@@ -22,17 +23,20 @@ const toCheck = [
 // main ....
 let globalValid = true
 toCheck.forEach( v => testExists(v) )
-if (!globalValid) {
-  const lines = envContents.split('\n').sort().join('\n')
-  console.log('Contents of .env file:')
-  console.log( lines)
+if (globalValid) {
+  console.log('Env check success')
+} else {
+  console.log('Env check fail')
+  // console.log('Contents of .env file:')
+  // console.log( envLines.join('\n'))
   process.exit(1)
 }
 // ... end main
 
 function testExists(key) {
-  const regexp = new RegExp(key + '=', 'g')
-  const matches = envContents.match(regexp);
+  const regexp = new RegExp('^' + key + '=', 'g')
+  const matches = envLines.filter(ev => null !== ev.match(regexp))
+  // console.log(key, matches)
   let isValid = Array.isArray(matches) && matches.length === 1
   if (!isValid) {
     console.log('Environment variable check FAIL --- ', key, '---- FAILED')
