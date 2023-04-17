c6035ffc63c735b01921e09c5ee13a039c5666b1
Bryan <13558676+bryan-gilbert@users.noreply.github.com>
2023-04-17 10:35:19 -0700

    Ehr Only Demo intro page

--- client/src/outside/views/EhrOnlyDemo.vue
+++ client/src/outside/views/EhrOnlyDemo.vue
@@ -1,26 +1,29 @@
 <template lang="pug">
-  div(class="is-centered")
+  div(class="is-centered ehr-only-demo")
     section(v-if="canAccessDemo", class="content")
       h2(class="has-text-centered") {{ ehrOnlyDemoText.title }}
       div
         div(v-text-to-html="ehrOnlyDemoText.intro")
       div(class="list-card list-element")
-        p Try a scenario about a fictitious patient, Erin Johns, during day 2 of their hospital stay. It is based on Case Study #2 in "Health Case Studies" (see reference below).
+        p
+          router-link(:to="{path: 'ehr', query: {demoOnlyKey: 'erin2Seed'}}") Erin JOHNS case study
+        p Select this scenario to view the EHR chart for a fictitious patient named Erin Johns. The content is based on case study #2 in "Health Case Studies" (see reference below).
         p.
-          The patient in case 2 has a complicated health history and is experiencing COPD exacerbation due to community acquired pneumonia. The interprofessional collaboration is role modelled between nursing, medical radiology, medical laboratory, and health care workers in the emergency department.
-        router-link(:to="{path: 'ehr', query: {demoOnlyKey: 'erin2Seed'}}") Erin JOHNS case study
+          Erin Johns is 74 years old with a complicated health history and is experiencing COPD exacerbation due to community acquired pneumonia. This scenario exemplifies the interprofessional collaboration between nursing, medical radiology, medical laboratory, and health care professionals in the emergency department.
 
       div(class="list-card list-element")
+        p
+          router-link(:to="{path: 'ehr/current/assessments/integumentary-assessment', query: {demoOnlyKey: 'wound'}}") Wound care
+          span &nbsp; (be sure to select the Wound tab)
         p.
           In 2020, the EdEHR team worked with members from the BC Provincial Nursing Skin and Wound Committee, on the development and evaluation of a standardized skin and wound assessment module. The completed module mirrors the nursing documentation requirements used across all BC Provincial Health Authorities.
         p The project included the development of a sample case study and sample assignment that illustrates best practices and encourages documentation that considers what other healthcare professionals need to know.
-        router-link(:to="{path: 'ehr/current/assessments/integumentary-assessment', query: {demoOnlyKey: 'wound'}}") Wound care
-        p Be sure to select the Wound tab.
 
       div(class="list-card list-element")
-        p A case study for hematology lab technologists where a learner normally completes a peripheral blood film (PBF) review based on the CBC analysis and on viewing a blood smear under a microscope.  In this case the PBF is completed to illustrate the whole activity.
-        p This is a sample of how the EdEHR can support lab technologist learners within an EHR context.  The EdEHR is evolving and, eventually, the parts that belong in a lab information system (LIS) will be visually separated from the EHR components.
-        router-link(:to="{path: 'ehr/med-lab/hematology', query: {demoOnlyKey: 'pbf'}}") PBF review
+        p
+          router-link(:to="{path: 'ehr/med-lab/hematology', query: {demoOnlyKey: 'pbf'}}") PBF review
+        p Select this case study to see a sample hematology lab technologist assignment. This case study shows the final result after a learner has completed a peripheral blood film (PBF) review based on the CBC analysis and on viewing a blood smear under a microscope.
+        p This sample shows how MedLab learners can experience a part of a lab information system.  The EdEHR is evolving and, eventually, a lab information system (LIS) will be visually separated from the EHR components.
 
       div(v-if="isDev", class="list-card list-element")
         router-link(:to="{path: 'ehr', query: {demoOnlyKey: 'devSeed'}}") Dev Seed
@@ -29,11 +32,12 @@
       p {{ ehrOnlyDemoText.noAccess }}
 
     section(class="content")
-      h2(class="has-text-centered") References
-      p Health Case Studies
+      h4 Health Case Studies
       p Subtitle: Toward Closing the Healthcare Communication Gap
-      p Authors: Glynda Rees; Rob Kruger; and Janet Morrison
-      a(href="https://pressbooks.bccampus.ca/healthcasestudies") https://pressbooks.bccampus.ca/healthcasestudies
+        br
+        span Authors: Glynda Rees; Rob Kruger; and Janet Morrison
+        br
+        a(href="https://pressbooks.bccampus.ca/healthcasestudies") https://pressbooks.bccampus.ca/healthcasestudies
 
 
 </template>
@@ -72,14 +76,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .content {
-    font-size: 1.2rem;
-      max-width: 1024px;
-      margin-left: auto;
-      margin-right: auto;
+@import '../../scss/definitions';
+.content {
+  font-size: 1.2rem;
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+}
+select {
+  width: 15rem;
+}
+@media screen and (min-width: $main-width-threshold3){
+  .ehr-only-demo {
+    margin-left: 4rem;
   }
-  select {
-    width: 15rem;
-  }
-
+}
 </style>
