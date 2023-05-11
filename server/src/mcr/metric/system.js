2efac012d3f761b192b37ac8f02ae34564590881
Bryan <13558676+bryan-gilbert@users.noreply.github.com>
2023-05-11 14:13:39 -0700

    system metrics -- add more details about the cp usage

--- server/src/mcr/metric/system.js
+++ server/src/mcr/metric/system.js
@@ -13,7 +13,7 @@ export default class System {
     this.ts = Date.now()
     const cpus = os.cpus()
     this.samples = []
-    this.sampleSize = 60
+    this.sampleSize = 24 * 60
     this.info = {
       platform: os.platform(),
       cpusCount: cpus.length,
@@ -33,9 +33,19 @@ export default class System {
   _record () {
     this.ts = Date.now()
     const freememPercentage = Math.round( 1000 * os.freemem() / os.totalmem()) / 10
+    const loadAvg1 = os.loadavg()[0]
+    const loadAvg5 = os.loadavg()[1]
     const loadAvg15 = os.loadavg()[2]
-    const usage = Math.round(loadAvg15 / this.info.cpusCount * 1000) / 10
-    this.samples.push({ts: this.ts, usage: usage, freeMemPc: freememPercentage})
+    const usage1 = Math.round(loadAvg1 / this.info.cpusCount * 1000) / 10
+    const usage5 = Math.round(loadAvg5 / this.info.cpusCount * 1000) / 10
+    const usage15 = Math.round(loadAvg15 / this.info.cpusCount * 1000) / 10
+    this.samples.push({
+      ts: this.ts,
+      usage1: usage1,
+      usage5: usage5,
+      usage15: usage15,
+      usage: usage15, // legacy but only need for a short while
+      freeMemPc: freememPercentage})
     if (this.samples.length > this.sampleSize) {
       this.samples.shift()
     }
