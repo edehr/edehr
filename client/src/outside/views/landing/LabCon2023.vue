9fb18533a61e59d0550c602c010525386fca465e
Bryan <13558676+bryan-gilbert@users.noreply.github.com>
2023-05-24 16:03:57 -0700

    Start a landing page

---
+++ client/src/outside/views/landing/LabCon2023.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  div(class='main-with-side-content')
+    div
+      h2 Welcome attendees of LabCon 2023
+      div
+        a(href='https://csmls.org/')
+          img(src='/assets/landing/labcon2023/csmls-logo.png', alt='CSMLS Org'        )
+      div
+        a(href='https://www.bcit.ca/study/health-sciences/')
+          img(src='/assets/images/BCIT-256.png', alt='BCIT Health Sciences'        )
+      hr
+      div
+        h2 Hematology Case Study
+
+        div(class="bSmears")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_003.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_004.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_006.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_013.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_018.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_020.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_022.jpg")
+          smear-image(iName="Slide18-212BONLANLE_Akosua_024.jpg")
+
+    collab-list
+</template>
+
+<script>
+import { appText } from '@/appText'
+import AppQuote from '@/app/components/AppQuote'
+import UiLink from '@/app/ui/UiLink'
+import CollabList from '@/outside/views/collaborations/collabList'
+import SmearImage from '@/outside/views/landing/SmearImage.vue'
+
+export default {
+  components: { SmearImage, CollabList, UiLink, AppQuote },
+  data () {
+    return {
+      appText: appText,
+    }
+  },
+}
+</script>
+<style lang='scss' scoped>
+@import '../../../scss/definitions';
+
+.bSmears {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
---
+++ client/src/outside/views/landing/SmearImage.vue
@@ -0,0 +1,37 @@
+<template lang='pug'>
+  a(:href='iPath', target="_blank", class='iLink')
+    img(:src='iPath', alt='bloodSmear', class='iImage')
+    div {{iName}}
+</template>
+
+<script>
+export default {
+  name: 'SmearImage',
+  props: {
+    iName: { type: String }
+  },
+  computed: {
+    iPath () { return '/assets/landing/labcon2023/' + this.iName}
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../../../scss/definitions';
+
+.iLink {
+  margin-right: 2rem;
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  & div {
+    align-self: center;
+  }
+}
+.iImage {
+  width: 200px;
+  align-self: center;
+}
+
+</style>
