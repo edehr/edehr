d389a7e55b2222ad92d2bbe5ac1243721f5a958b
Bryan <13558676+bryan-gilbert@users.noreply.github.com>
2023-10-11 08:38:03 -0700

    SysAdmin can create a batch of case studies

--- server/src/server/server.js
+++ server/src/server/server.js
@@ -4,10 +4,69 @@ import EhrApp from './app.js'
 import { version } from 'process'
 import { logError } from '../helpers/log-error'
 import { setupWebSocket } from './push-server'
+import fs from 'fs'
+import { ObjectId } from 'mongodb'
+import SeedData from '../mcr/seed/seed-data'
+import Consumer from '../mcr/consumer/consumer'
+
 const debug = require('debug')('server')
 
 if (!validateNode()) {
   process.exit(1)
+}
+
+/*
+ * This routine is run after the server is started. It can perform special work as needed.
+ *
+ * Below this routine will create seeds in all primary consumers based on the contents of
+ * the 'folderPath'. To use this just place json files in this directory that define the seeds
+ * you want. The routine will only create a seed if no other seed shares the same name. Hence,
+ * it will only import the seed once.
+ *
+ * Clearly this only works for administrators of the system who can place the json files in the folder.
+ *
+ * TO DO provide a means for users to import a block of seeds
+ */
+async function afterStartUp () {
+  console.log('afterStartup BEGIN')
+  const folderPath = './seedDefsToImport'
+  const consumers = await Consumer.find({is_primary: true},
+    {
+      oauth_consumer_key: 1,
+      tool_consumer_instance_name:1,
+      tool_consumer_info_product_family_code:1,
+      tool_consumer_instance_description:1,
+      lti_version: 1
+    },
+  )
+  const doImport = true
+  if(doImport) {
+    for(const consumer of consumers) {
+      console.log('Start work for consumer', consumer.oauth_consumer_key)
+      const consumerId = new ObjectId(consumer._id)
+      if (fs.existsSync(folderPath)) {
+        const dirList = fs.readdirSync(folderPath)
+        for (const p of dirList) {
+          const path = folderPath + '/' + p
+          const def = fs.readFileSync(path, 'UTF-8')
+          const obj = JSON.parse(def)
+          obj.toolConsumer = consumer._id
+          const existing = await SeedData.find({
+            toolConsumer: consumerId,
+            name: { $eq: obj.name }
+          })
+          if (existing.length === 0) {
+            console.log('Create seed ', obj.name)
+            await SeedData.create(obj)
+          } else {
+            console.log('Seed exists already', obj.name)
+          }
+        }
+      }
+      console.log('Done work for consumer', consumer.oauth_consumer_key)
+    }
+  }
+  console.log('afterStartup DONE')
 }
 
 const ehrApp = new EhrApp(configuration)
@@ -18,6 +77,7 @@ ehrApp.setup(configuration)
     if (validateNode()) {
       const appServer = app.listen(serverPort, () => {
         console.log('Server running...', serverPort)
+        afterStartUp()
       })
       setupWebSocket(appServer, app.authUtil)
     }
