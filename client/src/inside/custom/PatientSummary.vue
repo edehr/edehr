935811b71ddb2351d9020d1b13dc1035f0464641
Bryan <13558676+bryan-gilbert@users.noreply.github.com>
2024-04-26 14:23:01 -0700

    1. Add req IDs to all Lab Req.  2. Add medlab to Patient Summary

--- client/src/inside/custom/PatientSummary.vue
+++ client/src/inside/custom/PatientSummary.vue
@@ -14,20 +14,60 @@
         //td {{ truncate(patientData.allergies, 40) }}
         //td {{ truncate(patientData.risks, 40) }}
 
-    h2 Medication Orders
-    p.
-      These are the medication orders this patient has on their chart.  Note this list may
-      not include medications ordered via order sets.
-    table(cellspacing="0" cellpadding="0")
-      tr(v-for='med in patientData.medorders', :key='med. medicationOrdersTable_id')
-        td
-          qrcode-vue(:value="extractMedName(med.med_medication)", :size="qrsize", level="H", margin="5")
-        td  {{ extractMedName(med.med_medication) }}
-        td  {{ med.med_dose }}
-        td  {{ med.med_timing }}
-        td  {{ med.med_route }}
-        td  {{ med.med_scheduled }}
-        td {{ med.med_reason }}
+    div(v-if="isEhrShowing")
+      h2 Medication Orders
+      p.
+        These are the medication orders this patient has on their chart.  Note this list may
+        not include medications ordered via order sets.
+      table(cellspacing="0" cellpadding="0")
+        tr(v-for='med in patientData.medorders', :key='med. medicationOrdersTable_id')
+          td
+            qrcode-vue(:value="extractMedName(med.med_medication)", :size="qrsize", level="H", margin="5")
+          td  {{ extractMedName(med.med_medication) }}
+          td  {{ med.med_dose }}
+          td  {{ med.med_timing }}
+          td  {{ med.med_route }}
+          td  {{ med.med_scheduled }}
+          td {{ med.med_reason }}
+    div(v-if='isLisShowing')
+      h2 Accessioning
+      table(cellspacing="0" cellpadding="0")
+        thead
+          tr
+            th Accession Id
+            th Requisition Id
+            th Specimen Type
+            th Test panel
+        tbody
+          tr(v-for='acc in accData', :key='acc.accession_id')
+            td
+              qrcode-vue(:value="acc.accessionID", :size="qrsize", level="H", margin="5")
+            td  {{ acc.accRequisitionID }}
+            td  {{ acc.accSpecimenType }}
+            td  {{ acc.accTestPanel }}
+    div(v-if='labReqs')
+      h2 Lab Requisitions
+      table(cellspacing="0" cellpadding="0")
+        thead
+          tr
+            th Requisition Id
+            th Hematology
+            th Coagulation
+            th Cardiac
+            th Electrolytes
+            th Liver Function
+            th Renal Profile
+        tbody
+          tr(v-for='req in labReqs', :key='req.requisitions_id')
+            td
+              qrcode-vue(:value="req.req_id", :size="qrsize", level="H", margin="5")
+            td  {{ req.req_hematology }}
+            td  {{ req.req_cogagulation }}
+            td  {{ req.req_tropinin }}
+            td  {{ req.req_electrolytes }}
+            td  {{ req.req_liverFunction }}
+            td  {{ req.req_renalProfile }}
+      div labReqs {{labReqs}}
 </template>
 
 <script>
@@ -43,8 +83,18 @@ export default {
     ehrText () { return t18EhrText() },
     md () { return StoreHelper.getMergedData() },
     patientData () { return EhrPatient.patientData() },
+    isEhrShowing () { return StoreHelper.isEHR_Showing()},
+    isLisShowing () { return StoreHelper.isLIS_Showing()},
+    mData () {
+      let data = StoreHelper.getMergedData()
+      return JSON.parse(JSON.stringify(data))
+    },
+    accData () { return this.mData.medLabAccession?.mlAccessioning || {} },
+    labReqs () { return this.mData.labRequisitions?.requisitions || {} }
   },
   methods: {
+    extractAccIdNumber ( accRex ) { return accRex['accessionID']},
+    extractAccReqNumber ( accRex ) { return accRex['accRequisitionID']},
     extractMedName ( medMedication ) { return extractMedName (medMedication)},
     truncate (input, lim) {
       return input && input.length > lim ? `${input.substring(0, lim)}...` : input
